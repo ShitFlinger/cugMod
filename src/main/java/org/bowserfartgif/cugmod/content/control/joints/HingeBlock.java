a397ff84ee74b9f736154316c74204b2cb172e7c
adrien <Adrien123Millett@outlook.com>
2026-04-27 22:34:41 -0400

    gewegweg
    gwegew

--- src/main/java/org/bowserfartgif/cugmod/content/control/joints/HingeBlock.java
+++
@@ -1,9 +0,0 @@
-package org.bowserfartgif.cugmod.content.control.joints;
-
-import net.minecraft.world.level.block.Block;
-
-public class HingeBlock extends Block {
-    public HingeBlock(Properties properties) {
-        super(properties);
-    }
-}
